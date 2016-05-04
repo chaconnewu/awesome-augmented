31ecc2b4790d37ad2f389cd104ac4b7e20a678bc
Yu Wu <yuwu2@paypal.com>
2016-05-03 18:36:19 -0700

    Fix README.md

--- generating_cover_page.py
+++ generating_cover_page.py
@@ -49,42 +49,22 @@ def count_github_urls(url, input_directory='.'):
     filename = input_directory + urlparse(url).path.split('/')[-1] + '.md'
     f = open(filename, 'r')
 
-    try:
-        content = f.read()
-        soup = generate_soup(content)
-        github_count, all_count = generate_urls_ratio(soup)
-        print('%s has %d github urls, %d all' % (urlparse(url).path.split('/')[-1], github_count, all_count))
+    content = f.read()
+    soup = generate_soup(content)
 
-        lis = soup.find_all('li');
-        visited = set()
+    return generate_urls_ratio(soup)
 
-        for li in lis:
-            a = li.find_all('a')
+def generate_awsome_project_info(url):
+    query = "select stargazers_count, pushed_at from awesome_augmented where repo_url='%s'" % (url)
+    cur.execute(query)
+    rows = cur.fetchall()
+    if len(rows) == 1:
+        stars_count, updated_at = rows[0]
+        updated_at_datetime = parse(updated_at)
 
-            if len(a) > 0 and re.search('^https://github.com/[^/]+/[^/]+/?$', a[0]['href']):
-                repo_url = a[0]['href']
-                query = "select stargazers_count, pushed_at from awesome_augmented where repo_url='%s'" % (repo_url)
-                cur.execute(query)
-                rows = cur.fetchall()
-                if len(rows) == 1:
-                    stars_count, updated_at = rows[0]
-                    updated_at_datetime = parse(updated_at)
-
-                    updated_days_ago = (datetime.now(pytz.utc)- updated_at_datetime).days
-                    tag = soup.new_tag('sup')
-                    tag.string = '%d GitHub links in total %d links, &#9733 %d, pushed %d days ago ' % (github_count, all_count, stars_count, updated_days_ago)
-                    if a[0] in visited:
-                        continue
-                    else:
-                        visited.add(a[0])
-                        # a[0].insert_after(tag)
-                        li.insert(len(li.contents), tag)
-    except:
-        traceback.print_exc()
-    f = open(filename, 'w')
-
-    f.write(soup.prettify(formatter=None))
-    del f
+        updated_days_ago = (datetime.now(pytz.utc)- updated_at_datetime).days
+        return (stars_count, updated_days_ago)
+    return (None, None)
 
 def main():
     # awesome_url = 'https://raw.githubusercontent.com/sindresorhus/awesome/master/readme.md'
@@ -93,13 +73,32 @@ def main():
     # f.write(content)
     # del f
 
-    soup = generate_soup(open('new.md', 'r').read())
-    awesome_urls = generate_all_github_urls(soup)
+    soup = generate_soup(open('README.md', 'r').read())
+    lis = soup.find_all('li');
+    visited = set()
 
-    for url in awesome_urls:
-        count_github_urls(url, input_dir)
+    for li in lis:
+        a = li.find_all('a')
 
-    # attach_github_info('awesome.md')
+        if len(a) > 0 and re.search('^https://github.com/[^/]+/[^/]+/?$', a[0]['href']):
+            url = a[0]['href']
+            github_count, all_count = count_github_urls(url, input_dir)
+            stars_count, updated_days_ago = generate_awsome_project_info(url)
+            if not stars_count or not updated_days_ago:
+                continue
+
+            tag = soup.new_tag('sup')
+            tag.string = '%d GitHub repository, %d links Total, &#9733 %d, pushed %d days ago ' % (github_count, all_count, stars_count, updated_days_ago)
+            if a[0] in visited:
+                continue
+            else:
+                visited.add(a[0])
+                li.insert(len(li.contents), tag)
+
+
+    f = open('README.md', 'w')
+    f.write(soup.prettify(formatter=None))
+    del f
 
 
 if __name__ == '__main__':
